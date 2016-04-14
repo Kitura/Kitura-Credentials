aea040d4d9b1d7bd6063e54d4ee67c25d31c0eeb
Ira Rosen <IRAR@il.ibm.com>
2016-04-14 13:08:01 +0300

    Session authentication

--- Sources/Credentials/Credentials.swift
+++ Sources/Credentials/Credentials.swift
@@ -16,13 +16,17 @@
 
 import Kitura
 import KituraNet
+import KituraSession
 import LoggerAPI
 
 import Foundation
 
+import SwiftyJSON
+
 public class Credentials : RouterMiddleware {
     
-    var plugins = [CredentialsPluginProtocol]()
+    var tokenPlugins = [CredentialsPluginProtocol]()
+    var sessionPlugins = [String : CredentialsPluginProtocol]()
     public var options : [String:AnyObject]
     
     public convenience init () {
@@ -34,53 +38,84 @@ public class Credentials : RouterMiddleware {
     }
     
     public func handle(request: RouterRequest, response: RouterResponse, next: () -> Void) {
-        var pluginIndex = -1
-        
-        // Extra variable to get around use of variable in its own initializer
-        var callback: (()->Void)? = nil
-        
-        let callbackHandler = {[unowned request, unowned response, next] () -> Void in
-            pluginIndex += 1
-            if pluginIndex < self.plugins.count {
-                let plugin = self.plugins[pluginIndex]
-                plugin.authenticate(request, response: response, options: self.options,
-                    onSuccess: { userProfile in
-                        request.userProfile = userProfile
-                        next()
-                    },
-                    onFailure: {
-                        self.redirectUnauthorized(response)
-                        next()
-                    },
-                    onPass: {
-                        callback!()
-                    },
-                    inProgress: {
-                        self.redirectUnauthorized(response)
-                        next()
-                    }
-                )
+        if let session = request.session  {
+            if let _ = request.userProfile {
+                next()
+                return
             }
             else {
-                do {
-                    try response.status(HttpStatusCode.UNAUTHORIZED).end()
+                let userProfile = session["userProfile"]
+                if  userProfile.type != .Null  {
+                    if let name = userProfile["displayName"].string,
+                        let provider = userProfile["provider"].string,
+                        let id = userProfile["id"].string {
+                        request.userProfile = UserProfile(id: id, displayName: name, provider: provider)
+                        next()
+                        return
+                    }
                 }
-                catch {
-                    Log.error("Failed to send response")
-                }
-                next()
             }
+            
+            session["returnTo"] = JSON(request.originalUrl ?? request.url)
+            self.redirectUnauthorized(response)
+            next()
         }
-
-        callback = callbackHandler
-        callbackHandler()
+        else {
+            var pluginIndex = -1
+            
+            // Extra variable to get around use of variable in its own initializer
+            var callback: (()->Void)? = nil
+            
+            let callbackHandler = {[unowned request, unowned response, next] () -> Void in
+                pluginIndex += 1
+                if pluginIndex < self.tokenPlugins.count {
+                    let plugin = self.tokenPlugins[pluginIndex]
+                    plugin.authenticate(request, response: response, options: self.options,
+                                        onSuccess: { userProfile in
+                                            request.userProfile = userProfile
+                                            next()
+                        },
+                                        onFailure: {
+                                            self.redirectUnauthorized(response)
+                                            next()
+                        },
+                                        onPass: {
+                                            callback!()
+                        },
+                                        inProgress: {
+                                            self.redirectUnauthorized(response)
+                                            next()
+                        }
+                    )
+                }
+                else {
+                    do {
+                        try response.status(HttpStatusCode.UNAUTHORIZED).end()
+                    }
+                    catch {
+                        Log.error("Failed to send response")
+                    }
+                    next()
+                }
+            }
+            
+            callback = callbackHandler
+            callbackHandler()
+        }
     }
+    
     
     public func register (plugin: CredentialsPluginProtocol) {
-        plugins.append(plugin)
-        plugins[plugins.count - 1].usersCache = NSCache()
+        switch plugin.type {
+        case .Token:
+            tokenPlugins.append(plugin)
+            tokenPlugins[tokenPlugins.count - 1].usersCache = NSCache()
+        case .Session:
+            sessionPlugins[plugin.name] = plugin
+            sessionPlugins[plugin.name]!.usersCache = NSCache()
+        }
     }
-    
+
     private func redirectUnauthorized (response: RouterResponse, path: String?=nil) {
         let redirect : String?
         if let path = path {
@@ -107,13 +142,85 @@ public class Credentials : RouterMiddleware {
         }
     }
     
+    
+    private func redirectAuthorized (response: RouterResponse, path: String?=nil) {
+        let redirect : String?
+        if let path = path {
+            redirect = path
+        }
+        else {
+            redirect = options["successRedirect"] as? String
+        }
+        if let redirect = redirect {
+            do {
+               try response.redirect(redirect)
+            }
+            catch {
+                response.error = NSError(domain: "Credentials", code: 1, userInfo: [NSLocalizedDescriptionKey:"Failed to redirect successfuly authorized request"])
+            }
+        }
+    }
 
+    
+    public func authenticate (credentialsType: String, successRedirect: String?=nil, failureRedirect: String?=nil) -> RouterHandler {
+        return { request, response, next in
+            if let plugin = self.sessionPlugins[credentialsType] {
+                plugin.authenticate(request, response: response, options: self.options,
+                                    onSuccess: { userProfile in
+                                        if let session = request.session {
+                                            var profile = [String:String]()
+                                            profile["displayName"] = userProfile.displayName
+                                            profile["provider"] = credentialsType
+                                            profile["id"] = userProfile.id
+                                            session["userProfile"] = JSON(profile)
+                                        
+                                            var redirect : String?
+                                            if session["returnTo"].type != .Null  {
+                                                redirect = session["returnTo"].stringValue
+                                                session.remove("returnTo")
+                                            }
+                                            self.redirectAuthorized(response, path: redirect ?? successRedirect)
+                                            
+                                        }
+                                        next()
+                    },
+                                    onFailure: {
+                                        self.redirectUnauthorized(response, path: failureRedirect)
+                                        next()
+                    },
+                                    onPass: {
+                                        self.redirectUnauthorized(response, path: failureRedirect)
+                                        next()
+                    },
+                                    inProgress: {
+                                        next()
+                    }
+                )
+            }
+            else {
+                do {
+                    try response.status(HttpStatusCode.UNAUTHORIZED).end()
+                }
+                catch {
+                    Log.error("Failed to send response")
+                }
+                next()
+            }
+        }
+    }
+}
+
+
+public enum CredentialsPluginType {
+    case Token
+    case Session
 }
 
 
 public protocol CredentialsPluginProtocol {
     var name: String { get }
     var usersCache: NSCache? { get set }
+    var type: CredentialsPluginType { get }
     
     func authenticate (request: RouterRequest, response: RouterResponse, options: [String:AnyObject], onSuccess: (UserProfile) -> Void, onFailure: () -> Void, onPass: () -> Void, inProgress: () -> Void)
 }
