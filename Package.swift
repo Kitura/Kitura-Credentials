9f139ae169cf98aa9ff37a536102b9b77396cf26
Vadim Eisenberg <vadime@il.ibm.com>
2016-08-29 12:03:08 +0300

    updated dependency versions in Package.swift
    set version of https://github.com/IBM-Swift/Kitura.git to 0.28.0
    set version of https://github.com/IBM-Swift/Kitura-Session.git to 0.28.0

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ import PackageDescription
 let package = Package(
     name: "Kitura-Credentials",
     dependencies: [
-        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 0, minor: 27),
-        .Package(url: "https://github.com/IBM-Swift/Kitura-Session.git", majorVersion: 0, minor: 27)
+        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 0, minor: 28),
+        .Package(url: "https://github.com/IBM-Swift/Kitura-Session.git", majorVersion: 0, minor: 28)
     ]
 )
