03875a5e7840364fab48d36cc6425949bbd0d040
Shmuel Kallner <kallner@il.ibm.com>
2016-09-20 13:03:25 +0300

    IBM-Swift/Kitura#761 Updated Kitura and Kitura-Session dependencies

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ import PackageDescription
 let package = Package(
     name: "Kitura-Credentials",
     dependencies: [
-        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 0, minor: 32),
-        .Package(url: "https://github.com/IBM-Swift/Kitura-Session.git", majorVersion: 0, minor: 32)
+        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 0, minor: 33),
+        .Package(url: "https://github.com/IBM-Swift/Kitura-Session.git", majorVersion: 0, minor: 33)
     ]
 )
