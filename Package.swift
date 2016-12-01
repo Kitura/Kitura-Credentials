ae648b9074e055b698b41829fe69e8dd62b77f7d
Shmuel Kallner <kallner@il.ibm.com>
2016-12-01 19:19:10 +0200

    Updated dependencies

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ import PackageDescription
 let package = Package(
     name: "Kitura-Credentials",
     dependencies: [
-        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 2),
-        .Package(url: "https://github.com/IBM-Swift/Kitura-Session.git", majorVersion: 1, minor: 2)
+        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 3),
+        .Package(url: "https://github.com/IBM-Swift/Kitura-Session.git", majorVersion: 1, minor: 3)
     ]
 )
