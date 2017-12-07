33afb2c3009dc8bdb4b289a979791c8b502c6431
Aaron Liberatore <AaronLiberatore@aaron.austin.ibm.com>
2017-12-07 11:50:45 -0600

    auth

--- Sources/Credentials/Extensions.swift
+++ Sources/Credentials/Extensions.swift
@@ -1,5 +1,9 @@
 // https://stackoverflow.com/questions/27814519/swift-require-classes-implementing-protocol-to-be-subclasses-of-a-certain-cla
-public protocol AuthenticatedUser {
+
+/// Authenticated User - Protocol defining conformance to the user profile
+/// This has to be codable so we can send it to the user for symmetry
+
+public protocol AuthenticatedUser: Codable {
     init(_ profile: UserProfile)
     static func createCredentials() -> Credentials
     // These are the properties defined in UserProfile
@@ -10,6 +14,12 @@ public protocol AuthenticatedUser {
     var emails: [UserProfile.UserProfileEmail]? { get }
     var photos: [UserProfile.UserProfilePhoto]? { get }
     var extendedProperties: [String:Any] { get }
+}
+
+/// Wrapper holder the user profile and the codable object to be passed to the client
+public struct AuthenticatedObject<U: AuthenticatedUser, T: Codable>: Codable {
+  public let user: U?
+  public let object: T?
 }
 
 // Example of what a developer would need to in order to create an instance of AuthenticatedUser
@@ -29,5 +39,10 @@ class User: UserProfile, AuthenticatedUser {
         return extendedProperties["abc"] as? Int
     }
 
+  required convenience public init(_ profile: UserProfile) {
+    self.init(id: profile.id, displayName: profile.displayName,
+              provider: profile.provider, name: profile.name,
+              emails: profile.emails, photos: profile.photos,
+              extendedProperties: profile.extendedProperties)
+  }
 }
-
--- Sources/Credentials/RouterExtension.swift
+++ Sources/Credentials/RouterExtension.swift
@@ -4,13 +4,53 @@ import KituraContracts
 import LoggerAPI
 
 // This typealias would go into KituraContracts
+
 public typealias AuthCodableClosure<U: AuthenticatedUser, I: Codable, O: Codable> = (U, I, @escaping CodableResultClosure<O>) -> Void
+public typealias AuthCodableGetClosure<U: AuthenticatedUser, O: Codable> = (U, ([O]?, RequestError?) -> Void) -> Void
+public typealias CodableAuthArrayResultClosure<U: AuthenticatedUser, O: Codable> = (U?, [O]?, RequestError?) -> Void
 
 extension Router {
+  public func get<U: AuthenticatedUser, O: Codable>(_ route: String, handler: @escaping AuthCodableGetClosure<U, O>) {
+    getSafely(route, handler: handler)
+  }
+
+  fileprivate func getSafely<U: AuthenticatedUser, O: Codable>(_ route: String, handler: @escaping AuthCodableGetClosure<U, O>) {
+    let creds = U.createCredentials()
+    get(route, middleware: creds)
+    get(route) { request, response, next in
+      Log.verbose("Received GET type-safe authenticated request")
+      guard let profile = request.userProfile else {
+        response.status(.unauthorized)
+        next()
+        return
+      }
+      let user = U(profile)
+      // Define handler to process result from application
+      let resultHandler: CodableArrayResultClosure<O> = { result, error in
+        do {
+          let auth = AuthenticatedObject(user: user, object: result)
+          if let err = error {
+            let status = self.httpStatusCode(from: err)   //inaccessable due to private
+            response.status(status)
+          } else {
+            let encoded = try JSONEncoder().encode(auth)
+            response.status(.created)
+            response.send(data: encoded)
+          }
+        } catch {
+          // Http 500 error
+          response.status(.internalServerError)
+        }
+        next()
+      }
+      // Invoke application handler
+      handler(user, resultHandler)
+    }
+  }
     public func post<U: AuthenticatedUser, I: Codable, O: Codable>(_ route: String, handler: @escaping AuthCodableClosure<U, I, O>) {
         postSafely(route, handler: handler)
     }
-    
+
     fileprivate func postSafely<U: AuthenticatedUser, I: Codable, O: Codable>(_ route: String, handler: @escaping AuthCodableClosure<U, I, O>) {
         let creds = U.createCredentials()
         post(route, middleware: creds)
