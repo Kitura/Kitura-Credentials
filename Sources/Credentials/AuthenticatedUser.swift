63df42994ff45c7ea4d838eca44938c6afb7c400
David Jones <djones6@uk.ibm.com>
2018-04-26 13:56:18 +0100

    Skeleton definition for AuthenticatedUser

---
+++ Sources/Credentials/AuthenticatedUser.swift
@@ -0,0 +1,19 @@
+import Kitura
+import KituraContracts
+
+public final class AuthenticatedUser: Credentials, TypedMiddleware {
+    
+    private static let credentials = Credentials()
+    
+    public static func handle(request: RouterRequest, response: RouterResponse, completion: @escaping (AuthenticatedUser?, RequestError?) -> Void) {
+        // TODO: invoke handle for Credentials
+    }
+    
+    public static func describe() -> String {
+        return "TODO"
+    }
+    
+    
+    
+    
+}
